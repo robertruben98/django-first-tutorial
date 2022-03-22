f870d0c5862a21258f622fdc16c736184bfbfb60
Robe <robertruben98@gmail.com>
2022-03-22 15:47:17 -0500

    Creacion de endpoint consulta de datos

--- inmuebleslist_app/views.py
+++ inmuebleslist_app/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .models import Inmueble
 
 # Create your views here.
+def inmueble_list(request):
+    inmuebles = Inmueble.objects.all()
+    data = {
+        'inmuebles': list(inmuebles.values()),
+    }
+    
+    return JsonResponse(data)
