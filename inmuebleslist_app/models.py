b879d006f6912e632f353762aabb19d34071727b
Robe <robertruben98@gmail.com>
2022-03-23 22:31:26 -0500

    Relacion entre modelos Django

--- inmuebleslist_app/models.py
+++ inmuebleslist_app/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-class Edificacion(models.Model):
-    direccion = models.CharField(max_length=250)
-    pais = models.CharField(max_length=150)
-    descripcion = models.CharField(max_length=500)
-    imagen = models.CharField(max_length=900)
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.direccion
-    
-    
 class Empresa(models.Model):
     nombre = models.CharField(max_length=250)
     website = models.URLField(max_length=250)
@@ -21,3 +8,16 @@ class Empresa(models.Model):
     
     def __str__(self):
         return self.nombre
+
+
+class Edificacion(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
+    imagen = models.CharField(max_length=900)
+    active = models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="edificacion")
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.direccion
