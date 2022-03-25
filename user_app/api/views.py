b45991b76faf08fde2326b9489f8038df4099e5a
Robe <robertruben98@gmail.com>
2022-03-25 00:31:49 -0500

    Registro de usuarios con JWT

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
-from user_app import models
+# from user_app import models
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['POST', ])
@@ -24,8 +25,14 @@ def registration_view(request):
             data['response'] = 'El registro del usuario fue exitoso'
             data['username'] = account.username
             data['email'] = account.email
-            token = Token.objects.get(user=account).key
-            data['token'] = token
+            # token = Token.objects.get(user=account).key
+            # data['token'] = token
+            
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
 
         else:
             data = serializer.errors
