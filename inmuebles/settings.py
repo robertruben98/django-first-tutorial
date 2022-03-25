d91f095e3fa12e4a3520e79c77f08116be3d61bd
Robe <robertruben98@gmail.com>
2022-03-25 16:44:09 -0500

    Base de datos Postgres

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -81,10 +81,20 @@ AUTH_USER_MODEL = 'user_app.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'postgres',
+        'PASSWORD': 'jiku',
+        'HOST': 'localhost'
     }
 }
 
