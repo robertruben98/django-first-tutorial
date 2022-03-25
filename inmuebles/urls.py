05e4c17641458b17edc5193d01df5f2f9cc40630
Robe <robertruben98@gmail.com>
2022-03-24 20:40:02 -0500

    Crear usuario

--- inmuebles/urls.py
+++ inmuebles/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tienda/', include('inmuebleslist_app.api.urls')),
+    path('api-auth', include('rest_framework.urls')),
 ]
