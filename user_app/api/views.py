50004bf50136ebb8f720b907d1e601e72f51b5df
Robe <robertruben98@gmail.com>
2022-03-24 23:50:30 -0500

    Registr de nuevos usuarios y token de seguridad

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,12 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
+from user_app import models
 
 
-@api_view(['POST',])
+@api_view(['POST', ])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            data['response'] = 'El registro del usuario fue exitoso'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data = serializer.errors
+            
+        return Response(data)
