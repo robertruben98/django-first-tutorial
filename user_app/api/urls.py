95c877a67dc8a14dfb7099ccbbf94e557506ebff
Robe <robertruben98@gmail.com>
2022-03-24 23:22:19 -0500

    Login con tokens de seguridad

---
+++ user_app/api/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    path('login/', obtain_auth_token, name='login')
+]
