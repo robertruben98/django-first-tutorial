803e21f0322c0b2c1c3b0427b132d6dd4074eb72
Robe <robertruben98@gmail.com>
2022-03-24 23:56:45 -0500

    Logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,6 +3,14 @@ from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
 from user_app import models
+from rest_framework import status
+
+
+@api_view(['POST', ])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['POST', ])
